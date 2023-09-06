076dbcbe395f1609a75467949a4fcbfa3f039ac1
Qingyao Sun <qs234@cornell.edu>
2023-09-06 18:37:36 -0400

    quick save

--- compress.py
+++ compress.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from functools import partial
+from itertools import islice
 
 import jax
 import jax.numpy as jnp
+import numpy as np
+import fire
 import wandb
 
 from smolGPT.model import gpt2
@@ -16,18 +20,35 @@ from smolGPT.utils import (
 
 
 
-def svdvals(params, first_n_svd):
+def svdvals_fn(params, first_n_svd):
     def svd(path, leaf):
-        S = jnp.linalg.svd(leaf, compute_uv=False, hermitian=False)
-        Ssorted = jnp.sort(jnp.max(jnp.abs(S)))
-        return Ssorted[first_n_svd:]
+        if len(leaf.shape) < 2:
+            return None
+
+        print(path, leaf.shape)
+        r = min(leaf.shape)
+        result_shape = jax.ShapeDtypeStruct((r,), leaf.dtype)
+        S = jax.pure_callback(partial(np.linalg.svd, compute_uv=False, hermitian=False), result_shape, leaf)
+
+        # S = jnp.linalg.svd(leaf, compute_uv=False, hermitian=False)
+
+        return S[first_n_svd:]
 
     Smax = jax.tree_util.tree_map_with_path(svd, params)
     return Smax
 
 
-def compress(params, first_n_svd):
-    pass
+def compress(params, va, iterations, first_n_svd, n_head):
+    # svdvals = svdvals_fn(params, first_n_svd)
+
+    loader = islice(va, iterations)
+    inputs, _ = next(loader)
+
+    logits, activations = gpt2(inputs, **params, n_head=n_head)
+
+    def show(path, leaf):
+        print(path, leaf.shape)
+    jax.tree_util.tree_map_with_path(show, activations)
 
 
 def main(model_size: str = "124M",
@@ -41,17 +62,19 @@ def main(model_size: str = "124M",
     config = locals()
     encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
     context_length = hparams["n_ctx"]
+    n_head = hparams["n_head"]
 
     data_path = Path(data_dir)
     va = DataLoader(data_path/"val.bin", context_length, gradient_accumulation, batch_size)
 
-    wandb.init(project="smolGPT", config=config)
+    # wandb.init(project="smolGPT", config=config)
     params = compress(params,
                       va,
                       iterations,
-                      first_n_svd)
-    wandb.finish()
+                      first_n_svd,
+                      n_head)
+    # wandb.finish()
 
 
 if __name__ == "__main__":
-    pass
+    fire.Fire(main)
--- smolGPT/model.py
+++ smolGPT/model.py
@@ -4,21 +4,34 @@ import jax
 import jax.numpy as jnp
 
 
-def layer_norm(x, g, b, eps: float = 1e-5):
-    mean = jnp.mean(x, axis=-1, keepdims=True)
-    variance = jnp.var(x, axis=-1, keepdims=True)
-    return g * (x - mean) / jnp.sqrt(variance + eps) + b
+def layer_norm(act, g, b, eps: float = 1e-5):
+    mean = jnp.mean(act, axis=-1, keepdims=True)
+    variance = jnp.var(act, axis=-1, keepdims=True)
+    act_g = (act - mean) / jnp.sqrt(variance + eps)
+    out = act_g * g + b
+
+    activation = {"g": act_g}
+    return out, activation
 
 
-def linear(x, w, b, u = None, v = None):
-    if u is None and v is None:
-        return x @ w + b
+def linear(act, w, b, u = None, v = None):
+    out = act @ w + b
+    activation = {"w": act}
 
-    return x @ w + (x @ u) @ v + b
+    if u is not None or v is not None:
+        act_v = out @ u
+        out = out + act_v @ v
+        activation.update({"u": act, "v": act_v})
+
+    return out, activation
 
 
-def ffn(x, c_fc, c_proj):
-    return linear(jax.nn.gelu(linear(x, **c_fc)), **c_proj)
+def ffn(act, c_fc, c_proj):
+    out_fc, act_fc = linear(act, **c_fc)
+    out, act_proj = linear(jax.nn.gelu(out_fc), **c_proj)
+
+    activation = {"c_fc": act_fc, "c_proj": act_proj}
+    return out, activation
 
 
 def attention(q, k, v, mask):
@@ -26,26 +39,37 @@ def attention(q, k, v, mask):
     return jax.nn.softmax(affinity) @ v
 
 
-def mha(x, c_attn, c_proj, n_head):
-    *B, T, C = x.shape
-    x = linear(x, **c_attn)
-    q, k, v = jnp.split(x, 3, axis=-1)
+def mha(act, c_attn, c_proj, n_head):
+    *B, T, C = act.shape
+    out_attn, act_attn = linear(act, **c_attn)
+
+    q, k, v = jnp.split(out_attn, 3, axis=-1)
     q = q.reshape(*B, T, n_head, C // n_head).swapaxes(-3, -2)
     k = k.reshape(*B, T, n_head, C // n_head).swapaxes(-3, -2)
     v = v.reshape(*B, T, n_head, C // n_head).swapaxes(-3, -2)
-    causal_mask = (1 - jnp.tri(T, dtype=x.dtype)) * jnp.finfo(x.dtype).min
+    causal_mask = (1 - jnp.tri(T, dtype=act.dtype)) * jnp.finfo(act.dtype).min
     out_heads = attention(q, k, v, causal_mask)
-    x = out_heads.swapaxes(-3, -2).reshape(*B, T, C)
-    x = linear(x, **c_proj)
-    return x
+    out_heads = out_heads.swapaxes(-3, -2).reshape(*B, T, C)
+    out, act_proj = linear(out_heads, **c_proj)
+
+    activation = {"c_attn": act_attn, "c_proj": act_proj}
+    return out, activation
 
 
-def transformer_block(x, mlp, attn, ln_1, ln_2, n_head):
+def transformer_block(act, ln_1, attn, ln_2, mlp, n_head):
     # TODO: switch to FlashAttention? (adding LoRA might be tricker)
     #       https://github.com/lhao499/blockwise-parallel-transformer/issues/2
-    x = x + mha(layer_norm(x, **ln_1), **attn, n_head=n_head)
-    x = x + ffn(layer_norm(x, **ln_2), **mlp)
-    return x
+
+    out_ln_1, act_ln_1 = layer_norm(act, **ln_1)
+    out_attn, act_attn = mha(out_ln_1, **attn, n_head=n_head)
+    out_res_1 = act + out_attn
+
+    out_ln_2, act_ln_2 = layer_norm(out_res_1, **ln_2)
+    out_mlp, act_mlp = ffn(out_ln_2, **mlp)
+    out = out_res_1 + out_mlp
+
+    activation = {"ln_1": act_ln_1, "attn": act_attn, "ln_2": act_ln_2, "mlp": act_mlp}
+    return out, activation
 
 
 def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):
@@ -61,11 +85,15 @@ def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):
 
     # https://jax.readthedocs.io/en/latest/notebooks/autodiff_remat.html#practical-notes
     @partial(jax.checkpoint, policy=jax.checkpoint_policies.dots_with_no_batch_dims_saveable)
-    def f(x, block):
-        x = transformer_block(x, **block, n_head=n_head)
-        return x, None
+    def f(act_block, block):
+        act_block, attn_block = transformer_block(act, **block, n_head=n_head)
+        return act_block, attn_block
 
-    x = wte[inputs] + wpe[:inputs.shape[-1]]
-    x, _ = jax.lax.scan(f, x, blocks_transposed)
+    act = wte[inputs] + wpe[:inputs.shape[-1]]
+    out_blocks, attn_blocks = jax.lax.scan(f, act, blocks_transposed)
 
-    return layer_norm(x, **ln_f) @ wte.T
+    act_wte, attn_ln = layer_norm(out_blocks, **ln_f)
+    out = act_wte @ wte.T
+
+    activations = {"wte": act_wte, "wpe": None, "blocks": attn_blocks, "ln_f": attn_ln}
+    return out, activations
